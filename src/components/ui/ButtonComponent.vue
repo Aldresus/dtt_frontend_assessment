e118c423fda2aafce22d14a1ce72fa0bfaa884d7
hugo <hugochampy@gmail.com>
2023-07-20 11:53:57 +0200

    button component

---
+++ src/components/ui/ButtonComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    class="button button-icon no-border element-color-primary rounded flex"
+  >
+    <img
+      class="no-drag"
+      v-if="props.icon"
+      :src="getImageUrl()"
+      alt="button icon"
+      @click="props.onClick"
+    />
+    <span class="text-color-white"> {{ props.label }}</span>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: false,
+  },
+  onClick: {
+    type: Function,
+    required: true,
+  },
+});
+
+function getImageUrl() {
+  // This path must be correct for your file
+  let url = new URL(`@/assets/icons/`, import.meta.url);
+  return `${url}/${props.icon}`;
+}
+</script>
+
+<style scoped>
+button img {
+  height: 20px;
+  width: auto;
+}
+</style>
