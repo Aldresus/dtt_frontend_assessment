8a9a499416e1f17a1938daf05270012ba85a7a2e
hugo <hugochampy@gmail.com>
2023-07-21 01:43:58 +0200

    input component

---
+++ src/components/ui/InputComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex-column gap-1/4">
+    <div class="input-field-title">{{ props.title }}</div>
+    <input
+      class="input-field element-color-tertiary-1 no-drag rounded no-border input-field-text"
+      :placeholder="props.placeholder"
+      :type="props.type"
+      :value="input"
+      @input="input = $event.target.value"
+    />
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["title", "placeholder", "type", "modelValue"]);
+
+const emit = defineEmits(["update:modelValue"]);
+
+const input = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modelValue", value),
+});
+</script>
+
+<style scoped></style>
