3302dd843894bf929139057e2f808a82e6461a24
hugo <hugochampy@gmail.com>
2023-07-21 12:25:12 +0200

    responsiveness

--- src/pages/HousesCreatePage.vue
+++ src/pages/HousesCreatePage.vue
@@ -1,119 +1,125 @@
 <template>
-  <div class="flex-column gap-1">
+  <div class="flex-column gap-1/2 listing-creation">
     <RouterLink to="/houses" class="no-decoration">
       <ButtonComponent
-        class="text-color-primary no-bg no-padding"
+        class="text-color-primary desktop-only no-bg no-padding"
         icon-pos="start"
         label="Back to overview"
         icon="ic_back_grey@3x.png"
     /></RouterLink>
-    <h1>Create new listing</h1>
-    <div class="flex-column gap-1/2 listing-creation">
-      <form>
-        <div>
-          <InputComponent
-            title="Street name*"
-            placeholder="Enter the street name"
-            v-model="housingStore.selectedHouse.location.street"
-          />
-        </div>
-        <div class="flex gap-1">
-          <InputComponent
-            class="flex-grow"
-            title="House number*"
-            placeholder="Enter the house number"
-            v-model="housingStore.selectedHouse.location.houseNumber"
-          />
-          <InputComponent
-            class="flex-grow"
-            title="Addition (optional)"
-            placeholder="e.g. A"
-            v-model="housingStore.selectedHouse.location.houseNumberAddition"
-          />
-        </div>
-        <div>
-          <InputComponent
-            title="Postal code*"
-            placeholder="e.g. 1000 AA"
-            v-model="housingStore.selectedHouse.location.zip"
-          />
-        </div>
-        <div>
-          <InputComponent
-            title="City*"
-            placeholder="e.g. Utrecht"
-            v-model="housingStore.selectedHouse.location.city"
-          />
-        </div>
-        <div>
-          <ImageDropzoneComponent
-            title="Upload picture (PNG or JPG)*"
-            v-model="housingStore.selectedHouse.image"
-          />
-        </div>
-        <div>
-          <InputComponent
-            title="Price*"
-            placeholder="e.g. €150.000"
-            v-model="housingStore.selectedHouse.price"
-          />
-        </div>
-        <div class="flex gap-1">
-          <InputComponent
-            class="flex-grow w-full"
-            title="Size*"
-            placeholder="e.g. 60m2"
-            type="number"
-            v-model="housingStore.selectedHouse.size"
-          />
-          <DropdownComponent
-            class="flex-grow w-full"
-            title="Garage*"
-            placeholder="Select"
-            :options="garageOptions"
-            v-model="housingStore.selectedHouse.garage"
-          />
-        </div>
-        <div class="flex gap-1">
-          <InputComponent
-            class="flex-grow"
-            title="Bedrooms*"
-            type="number"
-            placeholder="Enter amount"
-            v-model="housingStore.selectedHouse.rooms.bedrooms"
-          />
-          <InputComponent
-            class="flex-grow"
-            title="Bathrooms*"
-            type="number"
-            placeholder="Enter amount"
-            v-model="housingStore.selectedHouse.rooms.bathrooms"
-          />
-        </div>
-        <div>
-          <InputComponent
-            title="Construction date*"
-            placeholder="Enter description"
-            v-model="housingStore.selectedHouse.constructionYear"
-          />
-        </div>
-        <div>
-          <TextAreaComponent
-            title="Description*"
-            placeholder="Enter description"
-            v-model="housingStore.selectedHouse.description"
-          />
-        </div>
-        <div class="flex gap-1 mt-1">
-          <div class="flex-grow w-full"></div>
-          <ButtonComponent
-            class="w-full flex-grow text-color-white element-color-primary"
-            label="POST"
-            @clicked="onPost"
-          />
-        </div>
-      </form>
+    <div class="flex align-items-center justify-content-between mb-1">
+      <RouterLink to="/houses" class="mobile-only no-decoration">
+        <ButtonComponent
+          class="text-color-primary no-bg no-padding"
+          icon-pos="start"
+          icon="ic_back_grey@3x.png"
+      /></RouterLink>
+      <h1>Create new listing</h1>
     </div>
+    <form>
+      <div>
+        <InputComponent
+          title="Street name*"
+          placeholder="Enter the street name"
+          v-model="housingStore.selectedHouse.location.street"
+        />
+      </div>
+      <div class="flex gap-1/2 flex-wrap">
+        <InputComponent
+          class="flex-grow"
+          title="House number*"
+          placeholder="Enter the house number"
+          v-model="housingStore.selectedHouse.location.houseNumber"
+        />
+        <InputComponent
+          class="flex-grow"
+          title="Addition (optional)"
+          placeholder="e.g. A"
+          v-model="housingStore.selectedHouse.location.houseNumberAddition"
+        />
+      </div>
+      <div>
+        <InputComponent
+          title="Postal code*"
+          placeholder="e.g. 1000 AA"
+          v-model="housingStore.selectedHouse.location.zip"
+        />
+      </div>
+      <div>
+        <InputComponent
+          title="City*"
+          placeholder="e.g. Utrecht"
+          v-model="housingStore.selectedHouse.location.city"
+        />
+      </div>
+      <div class="my-1">
+        <ImageDropzoneComponent
+          title="Upload picture (PNG or JPG)*"
+          v-model="housingStore.selectedHouse.image"
+        />
+      </div>
+      <div>
+        <InputComponent
+          title="Price*"
+          placeholder="e.g. €150.000"
+          v-model="housingStore.selectedHouse.price"
+        />
+      </div>
+      <div class="flex flex-wrap gap-1">
+        <InputComponent
+          class="flex-grow w-0"
+          title="Size*"
+          placeholder="e.g. 60m2"
+          type="number"
+          v-model="housingStore.selectedHouse.size"
+        />
+        <DropdownComponent
+          class="flex-grow w-0"
+          title="Garage*"
+          placeholder="Select"
+          :options="garageOptions"
+          v-model="housingStore.selectedHouse.garage"
+        />
+      </div>
+      <div class="flex flex-wrap gap-1/2">
+        <InputComponent
+          class="flex-grow w-0"
+          title="Bedrooms*"
+          type="number"
+          placeholder="Enter amount"
+          v-model="housingStore.selectedHouse.rooms.bedrooms"
+        />
+        <InputComponent
+          class="flex-grow w-0"
+          title="Bathrooms*"
+          type="number"
+          placeholder="Enter amount"
+          v-model="housingStore.selectedHouse.rooms.bathrooms"
+        />
+      </div>
+      <div>
+        <InputComponent
+          title="Construction date*"
+          placeholder="Enter description"
+          v-model="housingStore.selectedHouse.constructionYear"
+        />
+      </div>
+      <div>
+        <TextAreaComponent
+          title="Description*"
+          placeholder="Enter description"
+          v-model="housingStore.selectedHouse.description"
+        />
+      </div>
+      <div class="flex gap-1 mt-1">
+        <div class="flex-grow w-full"></div>
+        <ButtonComponent
+          class="w-full flex-grow text-color-white element-color-primary"
+          label="POST"
+          @clicked="onPost"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
@@ -144,5 +150,10 @@ const onPost = () => {
 <style scoped>
 .listing-creation {
   max-width: 30vw;
+}
+@media (max-width: 768px) {
+  .listing-creation {
+    max-width: 100%;
+  }
 }
 </style>
